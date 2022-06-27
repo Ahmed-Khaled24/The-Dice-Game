e71cdd06cb18ae68c5f290e0d58b563f29770903
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2022-06-27 13:14:55 +0200

    feat: add change_players_dice function to index.js
    a fucniton that takes two random generated numbers and change
    the photo of the dice of each player accordingly

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+var photos = ["dice1.png", "dice2.png", "dice3.png", "dice4.png", "dice5.png", "dice6.png"];
+
 function generate_random_int(){
     var random_number = Math.random() * 6 + 1;
     random_number = Math.floor(random_number);
     return random_number;
 }
 
-function get_the_winner(player1, player2){
-    if(player1 > player2)
-        return "player1";
+function get_the_winner(player1_dice, player2_dice){
+    if(player1_dice > player2_dice)
+        return "player 1";
     else 
-        return "player2";
+        return "player 2";
 }
 
+function change_players_dice(player1_dice, player2_dice){
+    var player1_photo = photos[player1_dice-1];
+    var player2_photo = photos[player2_dice-1];
+    document.getElementById("player1").setAttribute("src", "images/"+player1_photo);
+    document.getElementById("player2").setAttribute("src", "images/"+player2_photo);
+}
 
